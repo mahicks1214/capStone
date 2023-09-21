bc00845376550ab7ac650ed69fe2d2a845a435de
Laura <lspulscher@gmail.com>
2023-09-21 10:02:49 -0700

    added a nav bar, footer, and cleaned up the landing and user home pages.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,53 @@
-import React from "react";
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
-const Footer = () => (
-  <footer className="bg-light p-3 text-center">
-    <div className="logo" />
-    <p>
-      Sample project provided by <a href="https://auth0.com">Auth0</a>
-    </p>
-  </footer>
-);
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        SpaceTime Connections
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export default Footer;
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function StickyFooter() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+
+      <CssBaseline />
+
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+
+    </ThemeProvider>
+  );
+}
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -14,139 +14,126 @@ import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+// function Copyright() {
+//     return (
+//         <Typography variant="body2" color="text.secondary" align="center">
+//             {'Copyright © '}
+//             <Link color="inherit" href="https://mui.com/">
+//                 Your Website
+//             </Link>{' '}
+//             {new Date().getFullYear()}
+//             {'.'}
+//         </Typography>
+//     );
+// }
 
 function fetchReservations(setupcomingSpaces) {
-  fetch('http://localhost:8081/reservations')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      setupcomingSpaces(data.slice(0, 6));
-    })
-    .catch((error) => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
+    fetch('http://localhost:1000/reservations')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            setupcomingSpaces(data.slice(0, 6));
+        })
+        .catch((error) => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
 
 const defaultTheme = createTheme();
 
 export default function LandingPage() {
-  const [upcomingSpaces, setupcomingSpaces] = useState([]);
+    const [upcomingSpaces, setupcomingSpaces] = useState([]);
 
-  useEffect(() => {
-    fetchReservations(setupcomingSpaces);
-  }, []);
+    useEffect(() => {
+        fetchReservations(setupcomingSpaces);
+    }, []);
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <AppBar position="relative">
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <CssBaseline />
+            {/* <AppBar position="relative">
         <Typography variant="h6" color="inherit" noWrap>
           TimeSpace Connections
         </Typography>
-      </AppBar>
-      <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Upcoming Reservations!
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Below are reservations that are coming up soon! Book now to lock in your TimeSpace!
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >              
-            </Stack>
-          </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {upcomingSpaces.map((upcomingSpaces) => (
-              <Grid item key={upcomingSpaces.id} xs={12} sm={6} md={3}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
+      </AppBar> */}
+            <main>
+                {/* Hero unit */}
+                <Box
                     sx={{
-                      // 16:9
-                      pt: '56.25%',
+                        bgcolor: 'background.paper',
+                        pt: 8,
+                        pb: 6,
                     }}
-                    image={`https://source.unsplash.com/random?wallpapers&id=${upcomingSpaces.id}`}
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Training Course - {upcomingSpaces.meetingName}
-                    </Typography>
-                    <Typography>
-                      In Room - {upcomingSpaces.roomId}
-                    </Typography>
-                    <Typography>
-                      Training Description - {upcomingSpaces.meetingDescription}
-                    </Typography>
-                    <Typography>
-                      Start Time - {upcomingSpaces.meetingStart}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </Box>
-      {/* End footer */}
-    </ThemeProvider>
-  );
+                >
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            Upcoming Reservations!
+                        </Typography>
+                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                            Below are reservations that are coming up soon! Book now to lock in your TimeSpace!
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 4 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        >
+                        </Stack>
+                    </Container>
+                </Box>
+                <Container sx={{ py: 8 }} maxWidth="md">
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+                        {upcomingSpaces.map((upcomingSpaces) => (
+                            <Grid item key={upcomingSpaces.id} xs={12}>
+                                <Card
+                                    sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+                                >
+                                    <CardMedia
+                                        component="div"
+                                        sx={{
+                                            width: 200,
+                                            height: 140,
+                                            backgroundSize: 'cover'
+                                        }}
+                                        image={`https://source.unsplash.com/random?wallpapers&id=${upcomingSpaces.id}`}
+                                    />
+                                    <CardContent sx={{ flexGrow: 1 }}>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Training Course - {upcomingSpaces.meetingName}
+                                        </Typography>
+                                        <Typography>
+                                            In Room - {upcomingSpaces.roomId}
+                                        </Typography>
+                                        <Typography>
+                                            Training Description - {upcomingSpaces.meetingDescription}
+                                        </Typography>
+                                        <Typography>
+                                            Start Time - {upcomingSpaces.meetingStart}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Stack direction="column" spacing={1}>
+                                            <Button size="small">View</Button>
+                                            <Button size="small">Edit</Button>
+                                        </Stack>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+        </ThemeProvider>
+    );
 }
